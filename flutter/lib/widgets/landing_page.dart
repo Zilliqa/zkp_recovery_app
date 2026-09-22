0cfa2cca2e0273fbd3c5253ca1232fcdf7888319
Shawn <shawn@zilliqa.com>
2026-09-22 21:04:27 +0800

    bump v0.5.1

--- flutter/lib/widgets/landing_page.dart
+++ flutter/lib/widgets/landing_page.dart
@@ -40,7 +40,7 @@ class _LandingPageState extends State<LandingPage> {
             InfoCard(
               imageUrl: 'assets/images/zilliqa-full-teal.svg',
               title: 'Legacy Account Migration Notice',
-              subTitle: 'v0.5.0',
+              subTitle: 'v0.5.1',
               paragraphs: paragraphs,
             ),
             const SizedBox(height: 16),
